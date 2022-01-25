ad2f169811c967f26902e806442dfea89fe28108
Leandro <leandroperezb@gmail.com>
2022-01-25 09:58:18 -0300

    Fix test class in equo-app and updated gradle plugin

--- kotlin-react/settings.gradle.kts
+++ kotlin-react/settings.gradle.kts
@@ -4,11 +4,11 @@ include("src:main:ui")
 buildscript {
     repositories {
         gradlePluginPortal()
-        maven(url = "https://dl.equo.dev/gradle/0.6.0")
+        maven(url = "https://dl.equo.dev/gradle/0.6.2")
     }
 
     dependencies {
-        classpath("com.equo:com.equo.gradle.plugin:0.6.0")
+        classpath("com.equo:com.equo.gradle.plugin:0.6.2")
     }
 }
 
